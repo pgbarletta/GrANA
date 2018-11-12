351634332ac42b195338b0e43aee5534ca206c02
German <pbarletta@gmail.com>
2018-11-12 11:57:12 -0300

    Minor formatting changes.

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,19 @@ float rsltion = .1;
 int main(int argc, char **argv) {
     // Get positions and associated variables ready.
     if (argc != 4) {
-        std::cerr << "Usage: GrANA resolution in_pdb ou_pdb" << '\n';
+        std::cerr << "Usage: GrANA resolution in_pdb out_pdb" << '\n';
         return 0;
     }
     try {
         rsltion = std::stof(argv[1]);
     } catch (...) {
-        std::cerr << "Bad input resolution. Please specify a "
-                  << "number between .01 and 1" << '\n';
+        std::cerr
+            << "Bad input resolution. Please specify a number between .01 and 1"
+            << '\n';
     }
     std::vector<int> indices = {
         300, 600, 900, 1200, 1500, 1800, 1240, 400, 500, 700, 800, 1000, 1100};
 
-    //////
     GrANA::Molecule prote(argv[2]);
 
     GrANA::Triangulation incl_area(prote, indices);
