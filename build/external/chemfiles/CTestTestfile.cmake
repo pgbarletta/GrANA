d0237d972554f4f9f4c96617a65b2b008a31b19f
pgbarletta <pbarletta@gmail.com>
2020-01-28 22:24:02 -0300

    untracking build

--- build/external/chemfiles/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pbarletta/labo/18/GrANA/external/chemfiles
-# Build directory: /home/pbarletta/labo/18/GrANA/build/external/chemfiles
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external")
