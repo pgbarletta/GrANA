5ab224fde297775ba3828cda3d40dee63ff1c750
pgbarletta <pbarletta@gmail.com>
2021-03-08 12:13:53 -0300

    Add GridPoint_to_point() that displaces the input GridPoint by an origin vector (the way it should be). Removed PDB drawing member function from GridPoint class and made its member variables public.

--- include/GrANA/grid_primitives.hpp
+++ include/GrANA/grid_primitives.hpp
@@ -1,7 +1,7 @@
 #ifndef GrANA_GRID_PRIMITIVES_H
 #define GrANA_GRID_PRIMITIVES_H
 
-#include "GrANA/continuous.hpp"
+#include "GrANA/continuous_primitives.hpp"
 #include "GrANA/utils.hpp"
 #include <array>
 #include <cstdint>
@@ -28,29 +28,21 @@ public:
     GridPoint() = default;
 
     GridPoint(const grid_t x, const grid_t y, const grid_t z) noexcept :
-        _xyz{x, y, z} {}
+        _xyz {x, y, z} { }
 
     GridPoint(const Point &p, float const resolution) noexcept :
-        _xyz{cont_to_grid(p[0], resolution), cont_to_grid(p[1], resolution),
-            cont_to_grid(p[2], resolution)} {}
+        _xyz {cont_to_grid(p[0], resolution), cont_to_grid(p[1], resolution),
+            cont_to_grid(p[2], resolution)} { }
 
-    // Draw GridPoint as atom.
-    void draw(FILE *ou_fil, int idx, int resid, Vector const &orig_vtor,
-        float const resolution) const {
-        float const fx = grid_to_cont(_xyz[0], resolution) + orig_vtor[0];
-        float const fy = grid_to_cont(_xyz[1], resolution) + orig_vtor[1];
-        float const fz = grid_to_cont(_xyz[2], resolution) + orig_vtor[2];
-        fmt::print(ou_fil,
-            "{: <6}{: >5} {: <4s} {:3} {:1}{: >4}    "
-            "{:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}          {: >2s}\n",
-            "HETATM", idx, "H", "GPU", "A", resid, fx, fy, fz, 1.0, 0.0, "H");
-        return;
-    }
+    GridPoint(
+        const Point &p, float const resolution, const Point &offset) noexcept :
+        _xyz {cont_to_grid(p[0] - offset[0], resolution),
+            cont_to_grid(p[1] - offset[1], resolution),
+            cont_to_grid(p[2] - offset[2], resolution)} { }
 
     grid_t &operator[](int idx) { return _xyz[idx]; }
     grid_t operator[](int idx) const { return _xyz[idx]; }
 
-private:
     std::array<grid_t, 3> _xyz;
 };
 
@@ -67,6 +59,15 @@ inline Point GridPoint_to_point(
     return Point(grid_to_cont(in_point[0], resolution),
         grid_to_cont(in_point[1], resolution),
         grid_to_cont(in_point[2], resolution));
+}
+
+// Turn a grid point into a continuous point, given the resolution and an
+// origin.
+inline Point GridPoint_to_point(
+    GridPoint const &in_point, float const resolution, Point const &origin) {
+    return Point(grid_to_cont(in_point[0], resolution) + origin[0],
+        grid_to_cont(in_point[1], resolution) + origin[1],
+        grid_to_cont(in_point[2], resolution) + origin[2]);
 }
 
 } // namespace GrANA
