361fa09501fc7c321dc9330aa6f42e9e913adb7c
pgbarletta <pbarletta@gmail.com>
2021-03-12 20:00:08 -0300

    using new functions and classes on main.cpp. Meh

--- src/main.cpp
+++ src/main.cpp
@@ -7,33 +7,33 @@
 #include "GrANA/continuous.hpp"
 #include "GrANA/grid.hpp"
 #include "GrANA/utils.hpp"
+#include <GrANA/PDB.hpp>
 #include <GrANA/octree.hpp>
 
 int main(int argc, char **argv) {
 
-    auto[in_pdb, out_pdb, resolution] = GrANA::get_input(argc, argv);
+    auto [in_pdb, resolution, margin, out_pdb] = GrANA::get_input(argc, argv);
 
     std::vector<int> indices = {
         300, 600, 900, 1200, 1500, 1800, 1240, 400, 500, 700, 800, 1000, 1100};
 
-    GrANA::Molecule prote(in_pdb);
+    auto const [prote, lig] = GrANA::read_PDB(in_pdb);
 
-    GrANA::Triangulation incl_area(prote, indices);
-    incl_area.draw(out_pdb);
+    // GrANA::Triangulation incl_area(prote, indices);
+    // incl_area.draw(out_pdb);
 
-    // GrANA::GridMolecule Gprote(prote, resolution);
-    // Gprote.draw("-g1mtn.pdb");
+    GrANA::GridMolecule Gprote(prote, resolution, margin);
+    GrANA::GridMolecule Glig(lig, resolution, margin, Gprote._origin);
 
-    // printf("_x_min: %i\n", Gprote._x_min);
-    // printf("_y_min: %i\n", Gprote._y_min);
-    // printf("_z_min: %i\n", Gprote._z_min);
+    GrANA::draw_PDB(Gprote, "gprote.pdb");
+    GrANA::draw_PDB(Glig, "glig.pdb");
+    GrANA::draw_PDB(Glig._bbox, "bbox.pdb");
 
-    // printf("_x_max: %i\n", Gprote._x_max);
-    // printf("_y_max: %i\n", Gprote._y_max);
-    // printf("_z_max: %i\n", Gprote._z_max);
+    auto const in_bounding_box = get_atoms_in_bbox(Gprote, Glig._bbox);
+    GrANA::draw_selection_PDB(Gprote, "in_bbox.pdb", in_bounding_box);
 
     // auto mtx = GrANA::fill_grid_tetrahedron(Gprote, resolution);
-    // draw(out_pdb, mtx, Gprote._orig_vtor, resolution);
+    // draw(out_pdb, mtx, Gprote._origin, resolution);
 
     return 0;
 }
